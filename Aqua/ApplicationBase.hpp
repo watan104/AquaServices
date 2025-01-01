050986ba971870aed4031d5fdbe8ffd85cd794d3
watan104 <anoxywatan@gmail.com>
2025-01-01 16:04:49 +0300

    Uygulama ve ağ yapılandırma güncellemeleri
    ApplicationBase yapıcısında `SetConsoleTitleW` kaldırıldı ve `enet_initialize` eklendi. Başarısız olursa hata mesajı yazdırılıyor. `NetworkPool` nesnesi `g_pool` oluşturuldu ve üç sunucu eklendi. `ConfigBuilder` fonksiyonunda `config.txt` dosyasından yapılandırma parametreleri okunuyor. `ApplicationBase` sınıfına yeni özel üyeler eklendi ve bazı üyeler `std::string` türüne dönüştürüldü. `Main.cpp` dosyasından ilgili kodlar kaldırıldı ve `#include <cstdlib>` satırı kaldırıldı.

--- Aqua/ApplicationBase.hpp
+++ Aqua/ApplicationBase.hpp
@@ -6,15 +6,32 @@
 class ApplicationBase {
 public:
 	ApplicationBase() {
-
+		SetConsoleTitleW(L"Aqua Project");
+		if (enet_initialize() != 0) {
+			fmt::print(fg(fmt::color::black) | bg(fmt::color::crimson), "\"ApplicationBase\" An error occurred while initializing ENet.\n");
+		}
+		NetworkPool g_pool;
+		g_pool.Add("SERVER-ONE", 17777, 1024);
+		g_pool.Add("SERVER-TWO", 17091, 1024);
+		g_pool.Add("SERVER-THREE", 16999, 1024);
+		while (true) {
+			g_pool.Poll();
+		}
 	}
 	~ApplicationBase() = default;
 
 	bool ConfigBuilder() {
 		TextScanner t("config.txt");
 		if (not t.IsLoaded()) return false;
-		/*to-do*/
-		
+		this->server_name = t.GetParmString("server_name", 1);
+		this->main_port = t.GetParmUInt("main_port", 1);
+		this->sub_port = t.GetParmUInt("sub_port", 1);
+		this->last_port = t.GetParmUInt("last_port", 1);
+		this->database_name = t.GetParmString("database_name", 1).c_str();
+		this->database_url = t.GetParmString("database_url", 1).c_str();
+		this->database_pass = t.GetParmString("database_pass", 1).c_str();
+		t.Kill();
+		return true;
 	}	
 public:
 	std::string GetAppBasePath() const {
@@ -26,10 +43,15 @@ public:
 	std::string GetAppPath() const {
 		return app__path;
 	}
+private:	
+	std::string		server_name			{ NULL };
+	std::uint16_t	main_port			{ NULL };
+	std::uint16_t	sub_port			{ NULL };
+	std::uint16_t	last_port			{ NULL };
 private:
-	const char*		database_name		{ nullptr };
-	const char*		database_url		{ nullptr };
-	const char*		database_pass		{ nullptr };
+	std::string		database_name		{ NULL };
+	std::string		database_url		{ NULL };
+	std::string		database_pass		{ NULL };
 private:
 	std::string		app_base_path		{ "src/bin/data/" };
 	std::string		app_items_path		{ "src/bin/data/item/" };
--- Aqua/Main.cpp
+++ Aqua/Main.cpp
@@ -1,7 +1,6 @@
 #pragma region Include
 #include <enet/enet.h>
 #include <iostream>
-#include <cstdlib>
 #include <fmt/core.h>
 #include <fmt/color.h>
 #pragma endregion
@@ -9,21 +8,13 @@
 #pragma region Modules
 #include "NetworkPool.hpp"
 #include "ApplicationBase.hpp"
+//#include ""
 #pragma endregion
 
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Winmm.lib")
 
 auto main() -> int {
-	SetConsoleTitleW(L"Growtopia Server");
-	if (enet_initialize() != 0) return EXIT_FAILURE;
-	NetworkPool g_pool;
-	ApplicationBase* g_app = new ApplicationBase();
-	g_pool.Add("EPS", 17777, 1024);
-	g_pool.Add("APS", 17091, 1024);
-	g_pool.Add("GrowtopiaOS", 16999, 1024);
-	while (true) {
-		g_pool.Poll();
-	}
+	
 	
 }
